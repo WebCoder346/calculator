91f34da3675623e5253de2d6b72d023ad3d613fb
Saurav <sauravcoder1@gmail.com>
2024-12-24 19:17:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const boxes = document.querySelectorAll(".boxes");
-const equalBtn = document.querySelector(".equalBox");
-const clearBtn = document.querySelector(".clearBox");
-let input = document.querySelector(".input");
-let a = "";
-boxes.forEach((box) => {
-  box.addEventListener("click", (element) => {
-    a = a + element.target.innerText;
-    document.querySelector(".input").value = a;
+const display = document.querySelector(".input");
+
+Array.from(document.querySelectorAll(".box")).forEach(e => {
+  e.addEventListener("click", () => {
+    let inp = e.textContent.trim();
+    let num = "";
+    if (inp !== "=" & inp != "c" & inp !== "") {
+      display.value += inp;
+    }
+    if (inp === "=") {
+
+      display.value = display.value.replace("÷", "/");
+      display.value = display.value.replace("×", "*");
+      try {
+        display.value = eval(display.value);
+      } catch (e) {
+        display.value = "Error";
+      }
+    }
+    else if (inp == "c") {
+      display.value = "";
+      num = "";
+    }
+    else if (inp == "") {
+      display.value = display.value.length > 1 ? display.value.slice(0, -1) : '0';
+    }
   })
-})
-equalBtn.addEventListener("click", () => {
-  
-  input.value = eval(eval(a));
-  a = input.value;
-})
-clearBtn.addEventListener("click", () => {
-  a = "";
-  input.value = "0";
-})
-document.querySelector(".x").addEventListener("click", () => {
-  a = a + "*";
-  document.querySelector(".input").value = a;
 })
